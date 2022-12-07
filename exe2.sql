6cdbde91c06e87b08de7b34909de408bfda7b87e
gusgusz <gustavoguimaraescamps@gmail.com>
2022-12-07 20:52:04 -0300

    exe1 e exe2

---
+++ exe2.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes (nome, cpf, numero_compras) VALUES ('Renato Caldeira Falcão', '46761645912', 1);
+
+UPDATE clientes SET numero_compras=5 WHERE cpf='48769275911';
+DELETE FROM clientes WHERE cpf='98765432100';
+SELECT * FROM clientes LIMIT 3;
