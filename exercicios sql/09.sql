00c0488f8344070eaadeb457634b2d321741a9e0
gusgusz <gustavoguimaraescamps@gmail.com>
2022-12-08 15:11:02 -0300

    exercicios e desafios

---
+++ exercicios sql/09.sql
@@ -0,0 +1,3 @@
+UPDATE clientes SET cpf='04652651298' WHERE nome='Lucca Santarém Branco';
+SELECT id FROM produtos WHERE nome='Violão Lava ME 2';
+INSERT INTO compras (id_cliente, id_produto) VALUES (8, 10);
